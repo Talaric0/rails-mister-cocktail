f6625bd70aab893218088ff2237b3fa5dac45df4
bruno talarico <bruno.talarico@gmail.com>
2020-08-28 16:53:53 -0300

    you can upload pictures now

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,4 +1,5 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, only: :create
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
@@ -6,7 +7,7 @@ class DosesController < ApplicationController
 
   def create
     @dose = Dose.new(dose_params)
-
+    @dose.cocktail = set_cocktail
     if @dose.save
       redirect_to @dose.cocktail, notice: 'Dose was successfully created.'
     else
@@ -17,10 +18,15 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_url(@dose.cocktail_id), notice: 'Dose was successfully destroyed.'
+    redirect_to cocktail_url(@dose.cocktail), notice: 'Dose was successfully destroyed.'
   end
 
   private
+
+  def set_cocktail
+    Cocktail.find(params[:cocktail_id])
+  end
+  
 
   def dose_params
     params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
